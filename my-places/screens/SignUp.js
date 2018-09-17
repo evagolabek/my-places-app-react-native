63bc247312daf858266573ceba90f6f8715f4125
evagolabek <golabek.eva@gmail.com>
2018-09-17 22:05:08 +0200

    missed signup action

--- my-places/screens/SignUp.js
+++ my-places/screens/SignUp.js
@@ -5,6 +5,7 @@ import t from 'tcomb-form-native';
 import Person, { formOptions } from '../models/Person';
 
 import styles from './SignUp.styles';
+import signUp from '../actions/users/sign-up';
 
 export default class SignUp extends Component {
 	constructor(props) {
@@ -34,6 +35,8 @@ export default class SignUp extends Component {
     const newUser = form.getValue();
     if (!newUser) return;
     console.log(newUser);
+    signUp(newUser);
+    this.clearForm();
   }
   render() {
   	const Form = t.form.Form;
